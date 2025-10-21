bfef9fc76e58c055c3b9b2e425866dcd2e7404aa
hamza5645 <hamzaosama5645@outlook.com>
2025-10-21 12:11:20 +0300

    Created a POST API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
 
   @Get(':id/roles')
   findUserWithRoles(@Param('id', ParseIntPipe) id: number) {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Role } from '../roles/entities/role.entity';
 
 @Injectable()
 export class UsersService {
@@ -9,6 +11,17 @@ export class UsersService {
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
   ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const { roleIds, ...userData } = createUserDto;
+    const user = this.usersRepository.create(userData);
+
+    if (roleIds && roleIds.length > 0) {
+      user.roles = roleIds.map((id) => ({ id } as Role));
+    }
+
+    return this.usersRepository.save(user);
+  }
 
   async findOneWithRoles(id: number): Promise<User | null> {
     return this.usersRepository
