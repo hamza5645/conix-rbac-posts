7845ba73be8fc6add4d6d95317a8993e0eb3a140
hamza5645 <hamzaosama5645@outlook.com>
2025-10-28 00:03:32 +0300

    Removed dead API

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,15 +1,9 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  createUser(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
 
   @Get(':id/roles')
   findUserWithRoles(@Param('id', ParseIntPipe) id: number) {
